0365b08605f8f7ea64267def2da27ffb2154b060
vwenziel-ship-it <vwenziel@gmail.com>
2025-10-02 11:52:58 +0800

    Password & email both correct

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+// villeza wenziel A 1-D
 function signIn(){
     let email = document.getElementById('email').value;  
 let password = document.getElementById('password').value; 
@@ -17,5 +18,42 @@ if (password==="wenzie123"){
 }else{
     console.log(" Your password is incorrect.");     
 }
+}function signIn (){
+    //Email:vwenziel@gmail.com// Error Message: Your email is incorrect
+    //password:wenzie123//
+    let email = document.getElementById("email").value;
+ let password = document.getElementById("password").value;
+       //let erroEmail = document.getElementId("email errror");
+       //let erropassword=document.getElementById("password-error");
+ 
+
+console.log("You Enter: "+ " " + email);
+console.log("You Enter: "+ " " + password);
+
+if(email==="vwenziel@gmaill.com"){
+    console.log("Your email is incorrect")
+}else{
+    console.log("Your email is incorrect!")
+    //erroEmail.innerHTML="Your email is incorrect.";
+
 }
+if (password==="wenzie123"){
+    console.log("Your password is correct "+password)
+}else{
+console.log("Your password is incorrect!")
+//erropassword.innerHTML=" Your password is incorrect!"
+ }
+ if (login){
+    console.log("Your email and password is correct1");
+ }else{
+    console.log("Your email and password is incorrect")
+ }
+ }
+
+
+
+//1.if email and password is both correct- display a message "Your email and password is correct"
+//2.if email is correct but password is incorrect- display a message "Your password is in correct"
+//3.if email is incorrect and passwoed is correct- display a message "Your email is incorrect"
+//4.and if both email and password is incorrect - display a message "Your email and password is incorrect"
 }   
